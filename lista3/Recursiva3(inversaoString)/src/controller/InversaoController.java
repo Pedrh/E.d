90691c8aef1c6684f6e0500f46aa48fbc6ce3775
Pedro Silva <phbsilva21@gmail.com>
2023-08-16 17:27:08 -0300

    Atualização dos projetos da terceira lista
    Terminei a recursiva 3

--- lista3/Recursiva3(inversaoString)/src/controller/InversaoController.java
+++ lista3/Recursiva3(inversaoString)/src/controller/InversaoController.java
@@ -5,30 +5,13 @@ public class InversaoController {
 	public InversaoController() {
 		super();
 	}
-	public String Inversor(String palavra, int i,String inverso, int c) {
+	public String Inversor(String palavra, int i, int f) {
 		//condição de parada ==> quando c tiver o mesmo tamanho da palavra (parar na posição final)
-		if(c==i-1) {
-			return inverso;
+		if(f==1) {
+			return palavra.substring(i,f);
 		}else {
-			c +=1;
-			inverso = palavra.substring(c);
-			return Inversor(palavra, i, inverso, c) + Inversor(palavra, i, inverso, c);
+			return palavra.substring(i, f) + Inversor(palavra, i - 1, f - 1);
 		}
-	} //FALTA ENCONTRAR UMA FORMA DE IR DIMINUINDO O C PRA RETORNAR AS OUTRAS LETRAS NA SEGUNDA PARTE DA SOMA
-
+	} 
 }
-/*public class InversaoController {
-	public static void main(String[] args) {
-
-		// como usar a string
-		String teste = "exemplo";
-		int i = teste.length() - 1;
-		int c;
-		for(c=0; c<=i; c++) {
-			System.out.println(teste.substring(0, c)); 
-		}
-
-	}
-
-}*/
 
--- lista3/Recursiva3(inversaoString)/src/view/Principal.java
+++ lista3/Recursiva3(inversaoString)/src/view/Principal.java
@@ -6,11 +6,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		InversaoController ic = new InversaoController();
-		String palavra = "teste"; 
-		String inverso = "";
-		int i = palavra.length();
-		int c = 1;
-		String res = ic.Inversor(palavra, i, inverso, c);
+		String palavra = "paralelepipedo"; 
+		int fim = palavra.length();
+		int inicio = fim - 1;
+		String res = ic.Inversor(palavra, inicio, fim);
 		System.out.println(res);
 	}
 
